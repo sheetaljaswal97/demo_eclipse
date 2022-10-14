059faa5634565be9085376eec7cecfac4a6e5347
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2022-10-14 14:47:31 +0530

    This is second commit

---
+++ selenium/src/main/java/selenium/class2.java
@@ -0,0 +1,11 @@
+package selenium;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is sheetal");
+
+	}
+
+}
