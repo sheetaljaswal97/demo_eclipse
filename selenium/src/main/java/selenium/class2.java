8b4521c1fe13f657d3bfb3ac71cb965df5bf8fd6
Sheetal.jaswal <Sheetal.jaswal@gmail.com>
2022-10-16 21:15:36 +0530

    Third commit

--- selenium/src/main/java/selenium/class2.java
+++ selenium/src/main/java/selenium/class2.java
@@ -5,6 +5,8 @@ public class class2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My name is sheetal");
+		System.out.println("My name is sheetal");
+		System.out.println("My name is sheetal");
 
 	}
 
